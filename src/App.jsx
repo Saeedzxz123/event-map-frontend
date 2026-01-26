48c2d1e8174f6d19543f448306265d1cdaf17a38
Saeedzxz123 <saeedbh1190@gmail.com>
2026-01-26 11:45:51 +0300

    fix a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import Navbar from './components/Navbar'
-import ProtectedRoute from './components/ProtectedRoute'
 
 import Landing from './pages/Landing'
-import Login from './pages/Login'
 import Register from './pages/Register'
-import OTP from './pages/OTP'
-import Events from './pages/Events'
-import EventShow from './pages/EventShow'
-import Profile from './pages/Profile'
+import Login from './pages/login'
+
 
 const App = () => (
   <AuthProvider>
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
