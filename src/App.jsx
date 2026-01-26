d1fe6c800394606e1ca829c5f614622ba65f1601
Saeedzxz123 <saeedbh1190@gmail.com>
2026-01-26 14:05:33 +0300

    fix app.js and add otp route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-
+import { AuthProvider } from './context/AuthProvider'
 import Landing from './pages/Landing'
 import Register from './pages/Register'
 import Login from './pages/login'
+import OTP from './pages/OTP'
 
 
 const App = () => (
@@ -13,6 +13,8 @@ const App = () => (
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/otp" element={<OTP />} />
+
       </Routes>
     </Router>
   </AuthProvider>
